88d3d901de6bb42ca6821b7f95035d4a5f09198b
agile-proc-6339 <taevonjordan1@gmail.com>
2020-02-03 21:26:44 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
-
-def move(board, index, player)
-   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def move(board, index, x)
+  
 end
